ead7974ea0c8dc4909c0554b8c38f2ab4c6ae3fa
turquito70 <113914568+turquito70@users.noreply.github.com>
2024-07-17 20:43:13 -0400

    FullMath.sol

--- contracts/libraries/FullMath.sol
+++ contracts/libraries/FullMath.sol
@@ -1,5 +1,9 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.0 <0.8.0;
+/**
+* pragma solidity >=0.4.0 <0.8.0;
+* Chagnge by:
+*/
+pragma solidity >=0.4.0;
 
 /// @title Contains 512-bit math functions
 /// @notice Facilitates multiplication and division that can have overflow of an intermediate value without any loss of precision
